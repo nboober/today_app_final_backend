28fef6ee3e5d4af20da07e226cbc0f94d6bf4a06
Nick <nboober@terpmail.umd.edu>
2019-12-11 13:00:10 -0500

    finished my seed data

--- db/migrate/20191211161806_create_clothes.rb
+++ db/migrate/20191211161806_create_clothes.rb
@@ -3,8 +3,10 @@ class CreateClothes < ActiveRecord::Migration[6.0]
     create_table :clothes do |t|
       t.string :name
       t.string :location
-      t.string :weather_category
+      t.integer :weather_category
+      t.integer :temp_category
       t.string :clothes_type
+      t.string :image
       t.integer :user_id
 
       t.timestamps
