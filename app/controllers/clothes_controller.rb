a2eaf7d5f112841d5283344180fd5e72f6fb10a3
Nick <nboober@terpmail.umd.edu>
2019-12-13 10:53:34 -0500

    added functionality to update clothes

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -5,6 +5,12 @@ class ClothesController < ApplicationController
         render json: clothing
     end
 
+    def update
+        clothing = Clothe.find(params[:id])
+        clothing.update(clothing_params)
+        render json: clothing
+    end
+
     private
     def clothing_params
         params.require(:clothe).permit(:name, :location, :weather_category, :temp_category, :clothes_type, :image, :user_id)
