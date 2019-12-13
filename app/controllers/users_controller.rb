a3f91c9dcb60ca2d3fe1102a23639396dfd442be
Nick <nboober@terpmail.umd.edu>
2019-12-13 15:57:42 -0500

    added serialized data to index method users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
     def index
         users = User.all
-        render json: users
+        render json: users.to_json(to_serialized_json)
     end
 
     def create
