83e3d21c816f932c834d0052712a1a5dc8274633
Nick <nboober@terpmail.umd.edu>
2019-12-17 10:16:28 -0500

    updated seed data and migration table to allow for multiple seasons and weather conditions

--- db/migrate/20191211161806_create_clothes.rb
+++ db/migrate/20191211161806_create_clothes.rb
@@ -3,8 +3,8 @@ class CreateClothes < ActiveRecord::Migration[6.0]
     create_table :clothes do |t|
       t.string :name
       t.string :location
-      t.string :weather_category, :array => true, :default => []
-      t.string :temp_category, :array => true, :default => []
+      t.string :weather_category
+      t.string :temp_category
       t.string :clothes_type
       t.string :image
       t.integer :user_id
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,12 @@ OutfitClothe.destroy_all()
 
 nick = User.create(username: 'nboober', password: 'password', avatar: 'https://www.cvent-assets.com/csn-search-app-ui/assets/images/venue-card-placeholder__18v-Z.png', firstname: "Nick", lastname: "Boober")
 
-shirt = Clothe.create(name: "blue t-shirt", location: "closet 1, drawer 1", weather_category: ["any"], temp_category: ["any"], clothes_type: "shirt", image: "https://www.joinusonline.net/pub/media/catalog/product/cache/c9a86b5d6ed7765664725105f8d84377/t/s/tshirt_polyester_round_neck_royal_blue.jpg", user_id: nick.id)
-pants = Clothe.create(name: "brown pants", location: "closet 1, drawer 2", weather_category: ["any"], temp_category: ["any"], clothes_type: "pants", image: "https://www.impericon.com/media/catalog/product/l/g/lg_reell_cargoripstop_brown_pants.jpg", user_id: nick.id)
-shoes = Clothe.create(name: "black shoes", location: "closet 1, floor", weather_category: ["any"], temp_category: ["any"], clothes_type: "shoes", image: "https://scene7.zumiez.com/is/image/zumiez/pdp_hero/adidas-Xplorer-Core-Black-Shoes--_287999.jpg", user_id: nick.id)
-hat = Clothe.create(name: "fun hat", location: "hook by the front door", weather_category: ["any"], temp_category: ["any"], clothes_type: "hat", image: "https://www.happygabby.com/wp-content/uploads/2014/11/rainbow-sock-monkey-knit-hat.jpg", user_id: nick.id)
-belt = Clothe.create(name: "black belt", location: "closet 1, drawer 3", weather_category: ["any"], temp_category: ["any"], clothes_type: "belt", image: "https://images-na.ssl-images-amazon.com/images/I/71xnIXoB7YL._UX679_.jpg", user_id: nick.id)
-jewelry = Clothe.create(name: "watch", location: "top of main dresser", weather_category: ["any"], temp_category: ["any"], clothes_type: "jewelry", image: "https://www.hamiltonwatch.com/media/catalog/product/cache/1/small_image/9df78eab33525d08d6e5fb8d27136e95/H/8/H82315331_4.png", user_id: nick.id)
+shirt = Clothe.create(name: "blue t-shirt", location: "closet 1, drawer 1", weather_category: "any", temp_category: "any", clothes_type: "shirt", image: "https://www.joinusonline.net/pub/media/catalog/product/cache/c9a86b5d6ed7765664725105f8d84377/t/s/tshirt_polyester_round_neck_royal_blue.jpg", user_id: nick.id)
+pants = Clothe.create(name: "brown pants", location: "closet 1, drawer 2", weather_category: "any", temp_category: "any", clothes_type: "pants", image: "https://www.impericon.com/media/catalog/product/l/g/lg_reell_cargoripstop_brown_pants.jpg", user_id: nick.id)
+shoes = Clothe.create(name: "black shoes", location: "closet 1, floor", weather_category: "any", temp_category: "any", clothes_type: "shoes", image: "https://scene7.zumiez.com/is/image/zumiez/pdp_hero/adidas-Xplorer-Core-Black-Shoes--_287999.jpg", user_id: nick.id)
+hat = Clothe.create(name: "fun hat", location: "hook by the front door", weather_category: "any", temp_category: "any", clothes_type: "hat", image: "https://www.happygabby.com/wp-content/uploads/2014/11/rainbow-sock-monkey-knit-hat.jpg", user_id: nick.id)
+belt = Clothe.create(name: "black belt", location: "closet 1, drawer 3", weather_category: "any", temp_category: "any", clothes_type: "belt", image: "https://images-na.ssl-images-amazon.com/images/I/71xnIXoB7YL._UX679_.jpg", user_id: nick.id)
+jewelry = Clothe.create(name: "watch", location: "top of main dresser", weather_category: "any", temp_category: "any", clothes_type: "jewelry", image: "https://www.hamiltonwatch.com/media/catalog/product/cache/1/small_image/9df78eab33525d08d6e5fb8d27136e95/H/8/H82315331_4.png", user_id: nick.id)
 
 outfit1 = Outfit.create(name: "outfit 1", user_id: nick.id)
 
